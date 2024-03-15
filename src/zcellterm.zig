59736b28bf04c362c3dc2d0c80a08c7fdf3a97a2
Alexis Viguié <net@siphonay.fr>
2024-03-15 17:50:29 +0100

    Optimize memory management Swap to GPA from page allocator Now allocating the next generation array only once and using a memory swap between it and the current generation array

--- src/zcellterm.zig
+++ src/zcellterm.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 const builtin = @import("builtin");
 const getterminalsize = @import("getterminalsize.zig");
-const page_allocator = std.heap.page_allocator;
 
 fn printCells(cells: []u1, printNewline: bool) !void {
     const stdout = std.io.getStdOut().writer();
@@ -14,25 +13,22 @@ fn printCells(cells: []u1, printNewline: bool) !void {
         try stdout.print("\n", .{});
 }
 
-fn computeNextGen(cells: *const []u1, ruleset: [8]u1, size: u16) !void {
-    const next_gen = try page_allocator.alloc(u1, size);
-    defer page_allocator.free(next_gen);
-
-    for (cells.*, next_gen, 0..) |*cell, *next_gen_cell, index| {
-        const prev_cell = cells.*[(index + size - 1) % size];
-        const next_cell = cells.*[(index + 1) % size];
-        const neighborhood: u8 = (@as(u8, prev_cell) << 2) | (@as(u8, cell.*) << 1) | next_cell;
+fn computeNextGen(current_gen: []u1, next_gen: []u1, ruleset: [8]u1, size: u16) !void {
+    for (current_gen, next_gen, 0..) |cell, *next_gen_cell, index| {
+        const prev_cell = current_gen[(index + size - 1) % size];
+        const next_cell = current_gen[(index + 1) % size];
+        const neighborhood: u8 = (@as(u8, prev_cell) << 2) | (@as(u8, cell) << 1) | next_cell;
 
         next_gen_cell.* = ruleset[neighborhood];
     }
-
-    for (next_gen, cells.*) |*next_gen_cell, *cell|
-        cell.* = next_gen_cell.*;
 }
 
 pub fn main() !void {
-    const args = try std.process.argsAlloc(page_allocator);
-    defer std.process.argsFree(page_allocator, args);
+    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    const gp_allocator = general_purpose_allocator.allocator();
+
+    const args = try std.process.argsAlloc(gp_allocator);
+    defer std.process.argsFree(gp_allocator, args);
 
     const stderr = std.io.getStdErr().writer();
 
@@ -47,11 +43,14 @@ pub fn main() !void {
 
     const termsize = try getterminalsize.getTerminalSize();
 
-    const cells = try page_allocator.alloc(u1, termsize.col);
-    defer page_allocator.free(cells);
+    var current_gen = try gp_allocator.alloc(u1, termsize.col);
+    defer gp_allocator.free(current_gen);
 
-    @memset(cells, 0);
-    cells[termsize.col / 2] = 1;
+    var next_gen = try gp_allocator.alloc(u1, termsize.col);
+    defer gp_allocator.free(next_gen);
+
+    @memset(current_gen, 0);
+    current_gen[termsize.col / 2] = 1;
 
     const rule: u8 = std.fmt.parseInt(u8, args[1], 10) catch {
         try stderr.print("Please specify a rule between 0 and 255\n", .{});
@@ -63,11 +62,12 @@ pub fn main() !void {
     for (0..8) |index|
         ruleset[index] = if ((rule & (@as(u8, 1) << @intCast(index))) != 0) 1 else 0;
 
-    try printCells(cells, true);
+    try printCells(current_gen, true);
 
     for (0..termsize.row - 2) |iteration| {
-        try computeNextGen(&cells, ruleset, termsize.col);
+        try computeNextGen(current_gen, next_gen, ruleset, termsize.col);
+        std.mem.swap([]u1, &current_gen, &next_gen);
         // Windows’ prompt automatically prints a newline, so a final one is not needed.
-        try printCells(cells, (builtin.target.os.tag != .windows or iteration != termsize.row - 3));
+        try printCells(current_gen, (builtin.target.os.tag != .windows or iteration != termsize.row - 3));
     }
 }
